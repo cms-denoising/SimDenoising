bc3ee969c4b055bc37f9e14345a0a90f8efbda95
Kevin Pedro <kpedro88@gmail.com>
2020-06-03 20:47:10 -0500

    add configs for gen and sim

---
+++ Calo/python/optGenSim.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+defaults = {
+    "mineta": -3.,
+    "maxeta": 3.,
+    "minphi": -3.14159265359,
+    "maxphi": 3.14159265359,
+}
+
+options = VarParsing("analysis")
+options.register("particle", "electron", VarParsing.multiplicity.singleton, VarParsing.varType.string)
+options.register("mult", 1, VarParsing.multiplicity.singleton, VarParsing.varType.int) # number of particles
+options.register("energy", 1, VarParsing.multiplicity.singleton, VarParsing.varType.float)
+options.register("mineta", defaults["mineta"], VarParsing.multiplicity.singleton, VarParsing.varType.float)
+options.register("maxeta", defaults["maxeta"], VarParsing.multiplicity.singleton, VarParsing.varType.float)
+options.register("minphi", defaults["minphi"], VarParsing.multiplicity.singleton, VarParsing.varType.float)
+options.register("maxphi", defaults["maxphi"], VarParsing.multiplicity.singleton, VarParsing.varType.float)
+options.register("maxEventsIn", -1, VarParsing.multiplicity.singleton, VarParsing.varType.int)
+options.register("output", True, VarParsing.multiplicity.singleton, VarParsing.varType.bool)
+options.parseArguments()
+
+# choose particle
+options._pdgid = 0
+if options.particle=="electron": options._pdgid = 11
+elif options.particle=="positron": options._pdgid = -11
+elif options.particle=="photon": options._pdgid = 22
+else: raise ValueError("Unsupported particle: "+options.particle)
+
+# check options
+if options.maxEventsIn==-1: options.maxEventsIn = options.maxEvents
+
+# basic name definition
+nametmp = options.particle+"_energy"+str(options.energy)+"_mult"+str(options.mult)
+if any([defaults[x] != getattr(options,x) for x in defaults]):
+    nametmp = nametmp+"_eta"+str(options.mineta)+"to"+str(options.maxeta)+"_phi"+str(options.minphi)+"to"+str(options.maxphi)
+# gen name definition
+options._genname = "gen_"+nametmp+"_n"+str(options.maxEventsIn)
+# sim name definition
+options._simname = "sim_"+nametmp+"_n"+str(options.maxEvents)
+# ntuple name definition
+options._ntupname = "ntup_"+nametmp+"_n"+str(options.maxEvents)
---
+++ Calo/test/runSim.py
@@ -0,0 +1,121 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: --python_filename runSim.py --conditions auto:phase1_2018_realistic -n 10 --era Run2_2018 --eventcontent RAWSIM --relval 9000,100 -s SIM --datatier GEN-SIM --beamspot Realistic25ns13TeVEarly2018Collision --geometry DB:Extended --no_exec --filein file:step1.root --fileout file:step1.root
+import FWCore.ParameterSet.Config as cms
+from SimDenoising.Calo.optGenSim import options
+
+from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+
+process = cms.Process('SIM',Run2_2018)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:'+options._genname+'.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('--python_filename nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:'+options._simname+'.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.XMLFromDBSource.label = cms.string("Extended")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2018_realistic', '')
+
+process.g4SimHits.Watchers = cms.VPSet(
+    cms.PSet(
+        type = cms.string('EcalStepWatcher'),
+        volumes = cms.vstring("EBRY","EFRY"),
+    ),
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(options._ntupname+".root"),
+    closeFileFast = cms.untracked.bool(True),
+)
+
+# Path and EndPath definitions
+process.simulation_step = cms.Path(process.psim)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
