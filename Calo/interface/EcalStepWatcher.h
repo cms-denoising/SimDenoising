c78c69958653940dc0ecb2016340aa1df1a1a6d7
Kevin Pedro <kpedro88@gmail.com>
2021-07-14 14:36:08 -0500

    Revert "try to reset entire state (not just seeds)"
    This reverts commit e09727005d4fc2ca8d1de57f76be29cdd5c638a8.

--- Calo/interface/EcalStepWatcher.h
+++ Calo/interface/EcalStepWatcher.h
@@ -4,7 +4,6 @@
 //CMSSW headers
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h" 
-#include "SimDataFormats/RandomEngine/interface/RandomEngineState.h"
 #include "SimG4Core/Notification/interface/Observer.h"
 #include "SimG4Core/Watcher/interface/SimWatcher.h"
 
@@ -60,7 +59,7 @@ class EcalStepWatcher : public SimWatcher,
 		int ymax;
 		bool image_only;
 		bool reset_random;
-		std::vector<RandomEngineState> orig_state;
+		std::vector<std::uint32_t> orig_seeds;
 };
 
 #endif
